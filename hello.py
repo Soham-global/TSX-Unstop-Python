58b26c8951b77eb5750a6cce2379ec0c4a339c2b
Soham-global <magiciancoder1@gmail.com>
2025-05-16 11:18:01 +0530

    Adding hello.py and sample_script.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+# This is my first Python Program
+# Taking name as input from the user 
+name = input("Enter your name: ")
+
+# Greeting with name 
+print(f"Hello, {name}!")
+print("Welcome to TSX Python Internship")
+
+print("Hello from Soham")
---
+++ sample_script.py
@@ -0,0 +1,14 @@
+# This script greets the user and calculates the sum of two numbers
+# Ask the name of user
+name = input("Enter your name :")
+
+# Greeting
+print(f"Hello, {name}!")
+
+# Defining two numbers
+num1 = 15
+num2 = 80
+total = num1 + num2
+
+# Printing the sum of two numbers
+print(f"The sum of {num1} and {num2} is {total}")
